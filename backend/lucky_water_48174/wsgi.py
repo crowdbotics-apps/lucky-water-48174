63cbf5029ac16306e7ddb24d176bd5ad08407a7f
Crowdbotics <team@crowdbotics.com>
2024-05-30 09:18:16 +0000

    Initial commit

---
+++ backend/lucky_water_48174/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for lucky_water_48174 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "lucky_water_48174.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-water-48174",
+  url: "https://lucky-water-48174.botics.co",
+  api: "https://lucky-water-48174.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
